c645ec3d50add6373676cfe48ee34195714d8147
Stefania Stefansdottir <sstefansdottir88@gmail.com>
2020-04-03 14:30:02 -0500

    can create a new note but not delete

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,41 +1,40 @@
 const fs = require("fs");
 const util = require("util");
 
+module.exports = function (app) {
+  var notesDataArray = require("../db/db.json");
+  const writeFileAsync = util.promisify(fs.writeFile);
 
-
-module.exports = function(app) {
-    var notesDataArray = require("../db/db.json");
-    const writeFileAsync = util.promisify(fs.writeFile);
-
-app.get("/api/notes", function(req, res) {
+  app.get("/api/notes", function (req, res) {
     res.json(notesDataArray);
-});
-    
-app.post("/api/notes", function (req, res) {
-    var newNote = {text:req.body.text, title:req.body.title, id:notesDataArray.length}
+  });
+
+  app.post("/api/notes", function (req, res) {
+    var newNote = {
+      text: req.body.text,
+      title: req.body.title,
+      id: notesDataArray.length,
+    };
     notesDataArray.push(newNote);
-   var notesDataString = JSON.stringify(notesDataArray);
-    writeFileAsync("db/db.json", notesDataString).then(function (error) { 
-        if (error) {
-            console.log(error);
-        }
-        console.log("Note is written!", notesDataArray);
-        res.json(notesDataArray);
+    var notesDataString = JSON.stringify(notesDataArray);
+    writeFileAsync("db/db.json", notesDataString).then(function (error) {
+      if (error) {
+        console.log(error);
+      }
+      console.log("Note is written!", notesDataArray);
+      res.json(notesDataArray);
     });
-});
-app.delete("/api/notes/:id", function(req,res){
+  });
+  app.delete("/api/notes/:id", function (req, res) {
     console.log("received");
-    var keepArray = notesDataArray.filter(note => note.id!= req.params.id)
+    var keepArray = notesDataArray.filter((note) => note.id != req.params.id);
     var notesDataString = JSON.stringify(keepArray);
-    writeFileAsync("db/db.json", notesDataString).then(function (error) { 
-        if (error) {
-            console.log(error);
-        }
-        console.log("Note is written!", notesDataArray);
-        res.json(keepArray);
+    writeFileAsync("db/db.json", notesDataString).then(function (error) {
+      if (error) {
+        console.log(error);
+      }
+      console.log("Note is written!", notesDataArray);
+      res.json(keepArray);
     });
-
-})
-
-}
- 
+  });
+};
--- Develop/server.js
+++ Develop/server.js
@@ -3,11 +3,9 @@ const path = require("path");
 const fs = require("fs");
 const util = require("util");
 
-
 const app = express();
 
 const PORT = process.env.PORT || 8000;
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,36 +13,9 @@ app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 // app.use(express.static(__dirname + "/db"));
 
-
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
-}); 
-
-
-// app.get("/", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./public/index.html"));
-// });
-
-// app.get("/notes", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./public/notes.html"));
-// });
-
-
-// app.get("/api/notes", function(req, res) {
-//   res.json(notes);
-// });
-
-// app.post("/api/notes", function (req, res){
-//   var updatedNote = req.body;
-//   res.send(updatedNote)
-// });
-
-
-
-
- 
-
-  
+});
